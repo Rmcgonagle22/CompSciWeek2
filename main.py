1f11d6eed08a3cc876ed22bb00f645a16ceef250
Rmcgonagle22 <113391575+Rmcgonagle22@users.noreply.github.com>
2022-09-19 18:43:32 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+best_class = input("What is your favorite class?")
+loud_version = best_class.upper()
+# lets down case the string
+lower_version = best_class.lower()
+print(f"Your favorite class Is {loud_version}")
+print(f"and the lower case version is {lower_version}") # yes downcase here
+
+
